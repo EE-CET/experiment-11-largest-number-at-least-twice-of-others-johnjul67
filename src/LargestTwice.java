3e208bb6effcd243bd311756b58df3abc16f6c35
John <tve24el033@cet.ac.in>
2026-01-28 08:19:58 +0530

    commit2

--- src/LargestTwice.java
+++ src/LargestTwice.java
@@ -1,50 +1,45 @@
-import java.util.*;
+        import java.util.*;
 
-public class LargestTwice 
-{
-        static 	int max(int[] arr,int n)
-	{
-		if(n==1)
-                        return 0;
-                int m=0;
-                for (int i=0;i<n;i++) 
-                {
-                    if(arr[i]>=arr[m])
-                        m=i;
-                }
-		return m;
-	}
-        static boolean largetwice(int[] arr,int n,int m)
+        public class LargestTwice 
         {
-                boolean flag=false;
-                for(int i=0;i<n;i++)
+                static 	int max(int[] arr,int n)
                 {
-                        if(i!=m)
-                                if(arr[i]*2<=arr[m])
-                                        flag=true;
-                                else
-                                        flag=false;
+                        if(n==1)
+                                return 0;
+                        int m=0;
+                        for (int i=0;i<n;i++) 
+                        {
+                        if(arr[i]>=arr[m])
+                                m=i;
+                        }
+                        return m;
                 }
-                return flag;
+                static boolean largetwice(int[] arr,int n,int m)
+                {
+                        boolean flag=false;
+                        if (n == 1)
+                                flag=true;
+                        for(int i=0;i<n;i++)
+                        {
+                                if(i!=m)
+                                        if(arr[i]*2<=arr[m])
+                                                flag=true;
+                                        else
+                                                flag=false;
+                        }
+                        return flag;
+                }
+                public static void main(String[] args)
+                {
+                        Scanner sc=new Scanner(System.in);
+                        int n=sc.nextInt();
+                        int arr[]=new int[n];
+                        for(int i=0;i<n;i++)
+                                arr[i]=sc.nextInt();
+                        int m=max(arr, n);
+                        if(largetwice(arr, n, m))
+                                System.out.println(m);
+                        else
+                                System.out.println(-1);
+                }
         }
-        public static void main(String[] args)
-        {
-                Scanner sc=new Scanner(System.in);
-                int n=sc.nextInt();
-                int arr[]=new int[n];
-                for(int i=0;i<n;i++)
-                        arr[i]=sc.nextInt();
-                int m=max(arr, n);
-                if(largetwice(arr, n, m))
-                        System.out.println(m);
-                else
-                        System.out.println(-1);
-        }
-    
-        // TODO: Read n
-        // TODO: Read array elements
-        // TODO: Find the largest element and its index
-        // TODO: Check if largest >= 2 * every other element
-        // TODO: Print index or -1
-    
-}
